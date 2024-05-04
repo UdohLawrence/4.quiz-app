25c7af54060f87ff1f5190a87e946815f5c4db7a
UdohLawrence <lawrenceaudoh@gmail.com>
2024-05-04 08:34:24 +0100

    feature: header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" name="" id="" placeholder="Search...">
+    </header>
+    <div class="options-container">
+
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .container{
+    max-width: 960px;
+    margin: 0 auto;
+  }
+  header{
+    display: flex;
+    margin-top: 30px;
+    margin-bottom: 10px;
+    align-items: center;
+  }
+  header h1{
+    font-weight: bold;
+    margin-right: 30px;
+  }
+  header input{
+    border: none;
+    background-color: rgba(128,128,128,0.1);
+    padding: 10px;
+    border-radius: 5px;
+  }
+</style>
